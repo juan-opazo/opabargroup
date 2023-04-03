950f4d1a7a65353ae9bddacf507c97df738b27f7
Juan Opazo <dopazo178@gmail.com>
2023-04-02 19:43:11 -0500

    add new sector form

--- client/pages/registros.tsx
+++ client/pages/registros.tsx
@@ -10,42 +10,75 @@ interface RegistrationWeight {
     id: number;
     name: string;
     sector: number;
+    sector_name: string | undefined;
     date_created: string;
+  }
+
+interface Sector {
+    id: number;
+    name: string;
+    crop: string;
+    area: number;
   }
 
 const RegistrationPage = () => {
     const [rows, setRows] = React.useState<RegistrationWeight[]>([]);
     const [hasRegWeights, setHasRegWeights] = React.useState<Boolean>(true);
     const [regWeightSelected, setRegWeightSelected] = React.useState<RegistrationWeight | null>(null);
+    const [sectors, setSectors] = React.useState<Sector[]>([]);
+
+    // React.useEffect(() => {
+    //     getSectors();
+    //     getRegWeights();
+    // }, [sectors]);
+
+    const searchForSectorName = (id: number) => sectors.find((s: Sector) => s.id && s.id === id)?.name;
+
+    const getSectors = async () => {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/sector/sectors/`, {
+          method: "GET",
+          headers: { 
+            'Accept': 'application/json',
+            'Authorization': `Token ${localStorage.getItem('token')}` 
+          },
+        });
+        const data = await res.json();
+        if (res.ok) {
+          if (data.length === 0) {
+            return;
+          }
+          console.log(data);
+          setSectors([...data]);
+        } else {
+          console.error(data);
+        }
+      };
 
     const getRegWeights = async () => {
-        fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/registration-weight/registrationWeights/`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/registration-weight/registrationWeights/`, {
             method: "GET",
             headers: { 
             'Accept': 'application/json',
             'Authorization': `Token ${localStorage.getItem('token')}` 
             },
-        })
-        .then(res => {
-            res.json()
-            .then(data => {
-            if (res.ok) {
-                console.log(data);
-                if (data.length === 0) {
-                    setHasRegWeights(false);
-                return;
-                }
-                setRows([...data]);
-            } else {
-                console.error(data);
+        });
+        let data = await res.json()
+        if (res.ok) {
+            console.log(data);
+            if (data.length === 0) {
+                setHasRegWeights(false);
+            return;
             }
-    
+            console.warn(sectors);
+            data = data.map((reg: RegistrationWeight) => {
+                reg.sector_name = sectors.length ? searchForSectorName(reg.sector) : '';
+                return reg;
             })
-            .catch(err => {
-            console.error(err);
-            });
-        })
-        .catch(err => console.error(err));
+            console.log(data);
+            setRows([...data]);
+        } else {
+            console.error(data);
+        }
     }
 
     const handleVisibilityClick = (regWeight: RegistrationWeight) => {
@@ -54,7 +87,7 @@ const RegistrationPage = () => {
 
     const columns: GridColDef[] = [
         { field: 'name', headerName: 'Titulo', width: 100 },
-        { field: 'sector', headerName: 'Sector', width: 100 },
+        { field: 'sector_name', headerName: 'Sector', width: 100 },
         { field: 'date_created', headerName: 'Fecha', width: 130 },
         {
             field: 'visibility',
@@ -67,7 +100,10 @@ const RegistrationPage = () => {
         },
       ];
 
-    if (rows.length === 0 && hasRegWeights) getRegWeights();
+    if (rows.length === 0 && hasRegWeights) {
+        getSectors();
+        getRegWeights();
+    }
 
     return (
       <>
@@ -76,9 +112,6 @@ const RegistrationPage = () => {
             <DataGrid
                 rows={rows}
                 columns={columns}
-                // pageSize={5}
-                // rowsPerPageOptions={[5]}
-                // checkboxSelection
             />
         </div>
         { regWeightSelected ? <RegistrationWeights regWeightSelected={regWeightSelected}/> : null }
